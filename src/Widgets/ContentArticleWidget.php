057d3d54c1471b45924fc933b448c50e25b23d57
e-spin <gitlab@e-spin.de>
2023-02-24 08:18:16 +0100

    Rename widget - On the other hand, the template overwrites the untranslated

--- src/Widgets/ContentArticleWidget.php
+++ src/Widgets/ContentArticleWidget.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedcontentarticle.
  *
- * (c) 2012-2022 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,7 @@
  * @author     Andreas Dziemba <adziemba@web.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2022 The MetaModels team.
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedcontentarticle/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -59,7 +59,7 @@ class ContentArticleWidget extends AbstractWidget
      *
      * @var string
      */
-    protected $subTemplate = 'widget_contentarticle';
+    protected $subTemplate = 'widget_translatedcontentarticle';
 
     /**
      * Flag if the current entry has an id.
